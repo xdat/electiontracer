f4bf19f35bad4940cba582cd8c0a4087b0af3d32
Sedat Onur Orakoğlu <xdat26@gmail.com>
2023-03-26 11:53:59 +0300

    ..

---
+++ src/main/java/com/orakoglu/electiontracer/ElectiontracerApplication.java
@@ -0,0 +1,13 @@
+package com.orakoglu.electiontracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiontracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectiontracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orakoglu/electiontracer/ElectiontracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orakoglu.electiontracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiontracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
